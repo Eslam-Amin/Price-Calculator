ae93639c5d847a54c4e062786fdfe3a32a379832
Eslam Amin <eslam.amin.610@gmail.com>
2025-11-22 19:38:58 +0200

    Used the new separated conversion package

--- prices/prices.go
+++ prices/prices.go
@@ -4,7 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
+
+	"example.com/price-calculator/conversion"
 )
 
 type TaxIncludedPriceJob struct{
@@ -45,19 +46,15 @@ func (job *TaxIncludedPriceJob) LoadData(){
 		file.Close()
 		return 
 	}
-	prices := make([]float64, len(lines))
-	for lineIndex, line := range lines{
-		 floatPrice , err := strconv.ParseFloat(line, 64)
-		 if err != nil {
-			fmt.Println("Converting Price to float failed!")
-			fmt.Println(err)
-			file.Close()
-			return
-		}
-		prices[lineIndex] = floatPrice
+	prices, err :=conversion.StringsToFloats(lines)
+	if err != nil {
+		fmt.Println(err)
+		file.Close()
+		return 
 	}
-
+	
 	job.InputPrices = prices
+	file.Close()
 
 }
 
