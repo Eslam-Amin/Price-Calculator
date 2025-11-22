67a3df41e9891155104b45444f158b3ed98f44c3
Eslam Amin <eslam.amin.610@gmail.com>
2025-11-22 18:35:18 +0200

    Adding prices and taxRates

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+	var prices []float64 = []float64{10, 20, 30}
+	taxRatess := []float64{0, 0.07, 0.1, 0.15}
+
+	result := make(map[float64][]float64 )
+	for _, taxRate := range taxRatess{
+		taxIncludedPrices := make([]float64, len(prices))
+		for priceIndex, price := range prices{
+			taxIncludedPrices[priceIndex] = price * (1 + taxRate)
+		}
+
+		result[taxRate] = taxIncludedPrices
+	}
+
+	fmt.Print(result)
+
 }
