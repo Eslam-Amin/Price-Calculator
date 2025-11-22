74c5ae4a92e523ed29f2aa95b2310de880072db4
Eslam Amin <eslam.amin.610@gmail.com>
2025-11-22 19:16:20 +0200

    load prices data from a file, and add it to input Prices

--- prices/prices.go
+++ prices/prices.go
@@ -1,6 +1,11 @@
 package prices
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 type TaxIncludedPriceJob struct{
 	TaxRate float64
@@ -9,13 +14,47 @@ type TaxIncludedPriceJob struct{
 }
 
 
-func (job TaxIncludedPriceJob)Process(){
+func (job *TaxIncludedPriceJob)Process(){
+	job.LoadData()
 	result := make(map[string]float64)
 		for _, price := range job.InputPrices{
 			result[fmt.Sprintf("%.2f", price)] = price * (1 + job.TaxRate)
 		}
 
 		fmt.Println(result)
+}
+
+func (job TaxIncludedPriceJob) LoadData(){
+	file, err := os.Open("prices.txt")
+	if err != nil {
+		fmt.Println("Couldn't open file!")
+		fmt.Println(err)
+		return 
+	}
+	scanner := bufio.NewScanner(file)
+	var lines []string
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	err = scanner.Err()
+	if err != nil{
+		fmt.Println("Couldn't Read file content!")
+		fmt.Println(err)
+		file.Close()
+		return 
+	}
+
+	for lineIndex, line := range lines{
+		 floatPrice , err := strconv.ParseFloat(line, 64)
+		 if err != nil {
+			fmt.Println("Converting Price to float failed!")
+			fmt.Println(err)
+			file.Close()
+			return
+		}
+		job.InputPrices[lineIndex] = floatPrice
+	}
+
 }
 
 func NewTaxIncludedPriceJob (taxRate float64) *TaxIncludedPriceJob{
