8a0385525630f20634038c6e7537e01d943bf399
Eslam Amin <eslam.amin.610@gmail.com>
2025-11-22 19:48:29 +0200

    used filemanager package

--- prices/prices.go
+++ prices/prices.go
@@ -1,11 +1,10 @@
 package prices
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 
 	"example.com/price-calculator/conversion"
+	"example.com/price-calculator/filemanager"
 )
 
 type TaxIncludedPriceJob struct{
@@ -28,33 +27,19 @@ func (job *TaxIncludedPriceJob)Process(){
 }
 
 func (job *TaxIncludedPriceJob) LoadData(){
-	file, err := os.Open("prices.txt")
+
+	lines, err := filemanager.ReadLines("prices.txt")
 	if err != nil {
-		fmt.Println("Couldn't open file!")
 		fmt.Println(err)
-		return 
-	}
-	scanner := bufio.NewScanner(file)
-	var lines []string
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	err = scanner.Err()
-	if err != nil{
-		fmt.Println("Couldn't Read file content!")
-		fmt.Println(err)
-		file.Close()
 		return 
 	}
 	prices, err :=conversion.StringsToFloats(lines)
 	if err != nil {
 		fmt.Println(err)
-		file.Close()
 		return 
 	}
 	
 	job.InputPrices = prices
-	file.Close()
 
 }
 
