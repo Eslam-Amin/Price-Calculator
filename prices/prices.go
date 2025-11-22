2c63436aef5b3b92b866ea6fbc0a56b134b064fd
Eslam Amin <eslam.amin.610@gmail.com>
2025-11-22 18:56:36 +0200

    added process function to the struct

--- prices/prices.go
+++ prices/prices.go
@@ -1,5 +1,7 @@
 package prices
 
+import "fmt"
+
 type TaxIncludedPriceJob struct{
 	TaxRate float64
 	InputPrices []float64
@@ -7,6 +9,12 @@ type TaxIncludedPriceJob struct{
 }
 
 
+func (job TaxIncludedPriceJob)Process(){
+	result := make(map[string]float64)
+		for _, price := range job.InputPrices{
+			result[fmt.Sprintf("%.2f", price)] = price * (1 + job.TaxRate)
+		}
+}
 
 func NewTaxIncludedPriceJob (taxRate float64) *TaxIncludedPriceJob{
 
